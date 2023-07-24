391bde76093ae6d38e14e7c9d99345ac3f935af7
Utkarsh <utkarshgarg1803@gmail.com>
2023-07-25 00:48:27 +0530

    Added Note

--- src/screens/authentication/Login.js
+++ src/screens/authentication/Login.js
@@ -15,6 +15,7 @@ export default function Login() {
           <a href={loginEndpoint}>
             <div className="login-btn">LOG IN</div>
           </a>
+          <p className="note">NOTE : I am pleased to inform you that we have successfully hosted our project on Netlify. However, before someone can fully utilize our application, we need to grant specific permissions to users through our API. By adding their respective Spotify email accounts to our application, the authorization server will allow access. Failure to do so will result in an error code of 403 being returned.You can provide your spotify email by sending us a mail on - utkarshgarg1803@gmail.com</p>
     </div>
     );
     
